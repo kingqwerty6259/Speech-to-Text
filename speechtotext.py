d191ef564919e7a951839d27448081b0f2a3c1b9
kingqwerty6259 <68668995+kingqwerty6259@users.noreply.github.com>
2022-11-27 17:24:20 -0800

    Add files via upload

---
+++ speechtotext.py
@@ -0,0 +1,16 @@
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
+while True:
+	try:
+		with sr.Microphone() as source:
+			print("Connected")
+			audio = r.listen(source)
+			text = r.recognize_google(audio)
+			text = text.lower()
+			print(text)
+	except:
+		print(":|")
+		r = sr.Recognizer()
+		continue
